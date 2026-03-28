d7cb30e17a923a83a71b38fe6b8ec69fce8bed93
davicf <draivit124@gmail.com>
2026-03-28 14:50:51 -0400

    resolvendo issue 3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
+import { guestGuard } from './core/guards/guest.guard';
+import { authGuard } from './core/guards/auth.guards';
 
 export const routes: Routes = [
   {
     path: 'login',
+    canMatch: [guestGuard],
     loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)
   },
   {
     path: 'jogos',
+    canMatch: [authGuard],
     loadComponent: () => import('./features/jogos/list-jogos/list-jogos.component').then(m => m.ListJogosComponent)
   },
   {
---
+++ src/app/core/guards/guest.guard.ts
@@ -0,0 +1,18 @@
+
+import { inject } from '@angular/core';
+import { CanMatchFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const guestGuard: CanMatchFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+ 
+  if (authService.getToken()) {
+  
+    return router.createUrlTree(['/jogos']);
+  }
+
+  
+  return true;
+};
